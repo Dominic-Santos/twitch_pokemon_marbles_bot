eca2014b1e8dd2eee8983fcbf9005e8802f67b7c
Dominic-Santos <dominic.ah.santos@gmail.com>
2023-06-19 01:52:24 +0100

    test for baby

--- tests/test_pokedex.py
+++ tests/test_pokedex.py
@@ -30,25 +30,32 @@ def test_tiers():
 
 def test_fish():
     for pokemon in FISH_POKEMON:
-        assert pokedex.fish(pokemon) == True
+        assert pokedex.fish(pokemon)
     assert pokedex.fish("Pidgey") == False
 
 
 def test_have():
     for pokemon in dex_json["dex"]:
-        assert pokedex.have(pokemon["name"]) == True
+        assert pokedex.have(pokemon["name"])
 
 
 def test_have_extra():
-    assert pokedex.have("Wormadam (Sandy)") == True
+    assert pokedex.have("Wormadam (Sandy)")
     pokedex.pokemon["Wormadam"] = False
     assert pokedex.have("Wormadam (Sandy)") == False
-    assert pokedex.have("Aegislash (Blade)") == True
-    assert pokedex.have("Aegislash (Shield)") == True
+    assert pokedex.have("Aegislash (Blade)")
+    assert pokedex.have("Aegislash (Shield)")
     pokedex.pokemon["Aegislash"] = False
     assert pokedex.have("Aegislash (Blade)") == False
     assert pokedex.have("Aegislash (Shield)") == False
-    assert pokedex.have("Ho-Oh") == True
-    assert pokedex.have("Indeedee") == True
+    assert pokedex.have("Ho-Oh")
+    assert pokedex.have("Indeedee")
     assert pokedex.have("garbage") == FAIL_TO_FIND
-    assert pokedex.have("Mime Jr.") == True
+    assert pokedex.have("Mime Jr.")
+
+
+def test_baby():
+    assert pokedex.baby("Pichu")
+    assert pokedex.baby("Charmander")
+    assert pokedex.baby("Charmeleon") == False
+    assert pokedex.baby("Caterpie")
