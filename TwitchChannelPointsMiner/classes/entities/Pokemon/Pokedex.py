7b9e395269a9f7baf35207ece9940a0b32da44c8
Dominic-Santos <dominic.ah.santos@gmail.com>
2023-08-03 02:10:22 +0100

    getting evo data

--- TwitchChannelPointsMiner/classes/entities/Pokemon/Pokedex.py
+++ TwitchChannelPointsMiner/classes/entities/Pokemon/Pokedex.py
@@ -8,7 +8,7 @@ POKEMOVE_FILE = "pokemon_moves.json"
 STARTER_POKEMON = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert", "Turtwig", "Grotle", "Torterra", "Chimchar", "Monferno", "Infernape", "Piplup", "Prinplup", "Empoleon", "Snivy", "Servine", "Serperior", "Tepig", "Pignite", "Emboar", "Oshawott", "Dewott", "Samurott", "Chespin", "Quilladin", "Chesnaught", "Fennekin", "Braixen", "Delphox", "Froakie", "Frogadier", "Greninja", "Rowlet", "Dartrix", "Decidueye", "Litten", "Torracat", "Incineroar", "Popplio", "Brionne", "Primarina", "Grookey", "Thwackey", "Rillaboom", "Scorbunny", "Raboot", "Cinderace", "Sobble", "Drizzile", "Inteleon"]
 LEGENDARY_POKEMON = ["Articuno", "Zapdos", "Moltres", "Mewtwo", "Mew", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Celebi", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Uxie", "Mesprit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Reshiram", "Zekrom", "Landorus", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Type: Null", "Silvally", "Tapu Koko", "Tapu Lele", "Tapu Bulu", "Tapu Fini", "Cosmog", "Cosmoem", "Solgaleo", "Lunala", "Nihilego", "Buzzwole", "Pheromosa", "Xurkitree", "Celesteela", "Kartana", "Guzzlord", "Necrozma", "Magearna", "Marshadow", "Poipole", "Naganadel", "Stakataka", "Blacephalon", "Zeraora", "Meltan", "Melmetal", "Zacian", "Zamazenta", "Eternatus", "Kubfu", "Urshifu", "Zarude", "Regieleki", "Regidrago", "Glastrier", "Spectrier", "Calyrex", "Wyrdeer", "Kleavor", "Ursaluna", "Basculegion", "Sneasler", "Overqwil", "Type: Null"]
 FISH_POKEMON = ["Alomomola", "Angerphish", "Arctovish", "Arrokuda", "Barboach", "Barbubble", "Barraskewda", "Basculegion", "Basculin", "Blaufisch", "Bruxish", "Bubbayou", "Carvanha", "Chinchou", "Dragalge", "Eelektrik", "Elektross", "Feebas", "Finneon", "Goldeen", "Gorebyss", "Gyarados", "Horsea", "Huntail", "Kingdra", "Kyogre", "Lanturn", "Lumineon", "Luvdisc", "Magikarp", "Mantine", "Mantyke", "Milotic", "Mudkip", "Overqwil", "Qwilfish", "Relicanth", "Remoraid", "Seadra", "Seaking", "Sharpedo", "Skrelp", "Stunfisk", "Tynamo", "Wailmer", "Wailord", "Whiscash", "Wishiwashi", "Wooper", "Dracovish", "Clauncher", "Clawitzer"]
-FEMALE_POKEMON = [10025, 86320, 10143, 10144, 10262, 10284, 10285, 10286, 10287, 10288, 10292, 10295, 10302, 10309, 10316, 10319, 10325, 10340, 10350, 10352, 10359, 10360, 10361, 10362, 10365, 10368, 10370, 10372, 10373, 10375, 10378, 10379, 10381, 10382, 10384, 10385, 10387, 10388, 10391, 10392, 10396, 10404, 10418, 10422, 10423, 10424, 10427, 10429, 10438, 10441, 10442, 10445, 10446, 10449, 10451, 10452, 10453, 10454, 10455, 10456, 10467, 10468, 10470, 10471, 10540, 10543, 10544, 10545, 10553, 10331, 10369, 10505]
+FEMALE_POKEMON = [10025, 86320, 10143, 10144, 10262, 10284, 10285, 10286, 10287, 10288, 10292, 10295, 10302, 10309, 10316, 10319, 10325, 10340, 10350, 10352, 10359, 10360, 10361, 10362, 10365, 10368, 10370, 10372, 10373, 10375, 10378, 10379, 10381, 10382, 10384, 10385, 10387, 10388, 10391, 10392, 10396, 10404, 10418, 10422, 10423, 10424, 10427, 10429, 10438, 10441, 10442, 10445, 10446, 10449, 10451, 10452, 10453, 10454, 10455, 10456, 10467, 10468, 10470, 10471, 10540, 10543, 10544, 10545, 10553, 10331, 10369, 10505, 10303]
 THREE_EVOS = ["Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Pichu", "Nidoran-male", "Nidoran-female", "Cleffa", "Igglybuff", "Zubat", "Oddish", "Mankey", "Poliwag", "Abra", "Machop", "Bellsprout", "Geodude", "Magnemite", "Rhyhorn", "Happiny", "Horsea", "Mime Jr.", "Elekid", "Magby", "Porygon", "Dratini", "Chikorita", "Cyndaquil", "Totodile", "Togepi", "Mareep", "Azurill", "Hoppip", "Teddiursa", "Swinub", "Larvitar", "Treecko", "Torchic", "Mudkip", "Wurmple", "Lotad", "Seedot", "Ralts", "Slakoth", "Whismur", "Aron", "Budew", "Trapinch", "Duskull", "Spheal", "Bagon", "Beldum", "Turtwig", "Chimchar", "Piplup", "Starly", "Shinx", "Gible", "Snivey", "Tepig", "Oshawott", "Lillipup", "Pidove", "Roggenrola", "Timbur", "Tympole", "Sewaddle", "Venipede", "Sandile", "Gothita", "Solosis", "Vanillite", "Klink", "Tynamo", "Litwick", "Axew", "Pawniard", "Deino", "Chespin", "Fennekin", "Froakie", "Fletchling", "Scatterbug", "Flabébé", "Honedge", "Goomy", "Rowlet", "Litten", "Popplio", "Pipipek", "Grubbin", "Bounsweet", "Jangmo-o", "Cosmog", "Grookey", "Scorbunny", "Sobble", "Rookidee", "Blipbug", "Rolycoly", "Hatenna", "Impidimp", "Dreepy", "Sprigatito", "Fuecoco", "Quaxly", "Pawmi", "Smoliv", "Nacli", "Tinkatink", "Frigibax"]
 
 REGION_PREFIX = {
@@ -175,6 +175,27 @@ class Pokedex(object):
         poke = Pokemon(self.pokemon_stats[pokemon_id])
 
         return poke
+
+    def set_evolutions(self, pokemonid, data):
+        pokemon_id = str(pokemonid)
+        if pokemon_id in self.pokemon_stats:
+            if "evolves_to" not in self.pokemon_stats[pokemon_id]:
+                self.pokemon_stats[pokemon_id]["evolves_to"] = {}
+
+            for other_id in data:
+                level = 0
+                stones = []
+                for d in data[other_id]:
+                    if d["req"].startswith("Level"):
+                        level = int(d["req"].split(" ")[1])
+                    elif d["req"].endswith("tone"):
+                        stones.append({
+                            "amount": int(d["req"].split("x")[0]),
+                            "stone": d["req"].lower().split(" ")[1].replace("stone", " ").title()
+                        })
+                self.pokemon_stats[pokemon_id]["evolves_to"][other_id] = {"level": level, "stones": stones}
+                if other_id in self.pokemon_stats and ("evolves_from" not in self.pokemon_stats[other_id] or pokemon_id not in self.pokemon_stats[other_id]["evolves_from"]):
+                    self.pokemon_stats[other_id].setdefault("evolves_from", []).append(pokemon_id)
 
     def move(self, move_name):
         if move_name not in self.pokemon_moves:
