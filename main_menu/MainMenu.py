71f1afee8302b3a862854a6f877d162da882105c
Dominic-Santos <dominic.ah.santos@gmail.com>
2023-08-19 05:16:57 +0100

    all stats funcs abstracted

--- main_menu/MainMenu.py
+++ main_menu/MainMenu.py
@@ -2,9 +2,10 @@ import tkinter
 from tkinter import font as TKFont
 import subprocess
 from time import sleep
+from datetime import datetime
 
 from TwitchChannelPointsMiner.classes.ChatUtils import DISCORD_STATS
-from TwitchChannelPointsMiner.classes.chat_threads.DailyTasks import stats_computer
+from TwitchChannelPointsMiner.classes.chat_threads.DailyTasks import stats_computer, battle_summary, check_finish_pokedex
 
 from .Utils import (
     clear_widgets,
@@ -23,7 +24,7 @@ class MainMenu():
         font = TKFont.Font(size=15)
         self.button_conf = {
             "height": 2,
-            "width": 10,
+            "width": 13,
             "font": font
         }
         self.can_update = False
@@ -33,7 +34,7 @@ class MainMenu():
 
     def page_main_menu(self):
         clear_widgets(self.app)
-        self.app.geometry("350x350")
+        self.app.geometry("400x425")
         f = tkinter.Frame(self.app)
         f2 = tkinter.Frame(self.app)
         padding = {
@@ -47,6 +48,8 @@ class MainMenu():
         tkinter.Button(f, text="Settings", command=self.page_settings, **self.button_conf).grid(row=1, column=1, **padding)
         tkinter.Button(f, text="Missions", command=self.page_missions, **self.button_conf).grid(row=2, column=0, **padding)
         tkinter.Button(f, text="Bag Stats", command=self.bag_stats, **self.button_conf).grid(row=2, column=1, **padding)
+        tkinter.Button(f, text="Battle Stats", command=self.battle_stats, **self.button_conf).grid(row=3, column=0, **padding)
+        tkinter.Button(f, text="Pokedex Stats", command=self.pokedex_stats, **self.button_conf).grid(row=3, column=1, **padding)
         tkinter.Button(f2, text="Exit", command=self.close, **self.button_conf).grid(row=0, column=0, **padding)
         f.pack(anchor="center")
         f2.pack(anchor="center")
@@ -79,6 +82,15 @@ class MainMenu():
     def bag_stats(self):
         sync_all()
         discord_msg = stats_computer(POKEMON, POKEMON.pokedex.stats)
+        POKEMON.discord.post(DISCORD_STATS, discord_msg)
+
+    def pokedex_stats(self):
+        sync_all()
+        discord_msg = check_finish_pokedex(POKEMON, POKEMON.pokedex.stats)
+        POKEMON.discord.post(DISCORD_STATS, discord_msg)
+
+    def battle_stats(self):
+        discord_msg = battle_summary(datetime.now().date())
         POKEMON.discord.post(DISCORD_STATS, discord_msg)
 
     def load(self):
