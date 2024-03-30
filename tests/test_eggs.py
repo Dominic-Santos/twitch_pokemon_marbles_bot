5d20267b6329f70f68d592afdafa6e9c1ac8c39b
Dominic-Santos <dominic.ah.santos@gmail.com>
2024-03-30 13:43:03 +0000

    egg tests

--- tests/test_eggs.py
+++ tests/test_eggs.py
@@ -1,4 +1,13 @@
-from . import PokemonComunityGame, load_inventory_data, Pokemon
+from datetime import datetime
+
+from . import (
+    Chat,
+    load_inventory_data,
+    load_mission_data,
+    load_pokedex,
+    Pokemon,
+    PokemonComunityGame,
+)
 
 PCG = PokemonComunityGame()
 
@@ -75,3 +84,80 @@ def test_egg_trade_reasons():
     buddy.pokedex_id = 999004
     reasons = PCG.egg_trade_reasons(poke, buddy)
     assert len(reasons) == 3
+
+
+def test_chat_egg_functions(monkeypatch):
+    missions = load_mission_data("14")
+    inventory = load_inventory_data("01")
+    pokedex = load_pokedex()
+    computer = {
+        "allPokemon": [
+            {
+                "id": 28031618,
+                "lvl": 14,
+                "nickname": "\u2b50Bulbasaur",
+                "current_hp": 38,
+                "max_hp": 38,
+                "locked": False,
+                "pokedexId": 1,
+                "order": 1,
+                "isShiny": False,
+                "publicBattleBanned": False,
+                "isBuddy": True,
+                "avgIV": 20.333333333333332,
+                "sellPrice": 627,
+                "caughtAt": str(datetime.utcnow()),
+                "hpPercent": 100,
+                "specialVariant": 0,
+                "baseStats": 318,
+                "speed": 45,
+                "special_defense": 65,
+                "special_attack": 65,
+                "defense": 49,
+                "attack": 49,
+                "hp": 45,
+                "name": "Bulbasaur"
+            }
+        ]
+    }
+
+    PCG.missions.set(missions)
+    PCG.inventory.set(inventory)
+    PCG.pokedex.set(pokedex)
+    PCG.computer.set(computer)
+
+    monkeypatch.setattr(Chat, "POKEMON", PCG)
+
+    # 'username', 'token', 'channel', 'get_pokemon_token', 'pcg'
+    client = Chat.ClientIRCPokemon('username', 'token', 'channel', 'get_pokemon_token', True)
+    poke_obj, poke_bag = client.check_got_dragon_egg()
+    assert poke_bag is None
+    assert poke_obj is None
+
+    # change bulbasaur to egg
+    computer["allPokemon"][0].update({
+        "pokedexId": 999000,
+        "name": "Dragon Egg",
+    })
+    PCG.computer.set(computer)
+    poke_obj, poke_bag = client.check_got_dragon_egg()
+    assert poke_bag == computer["allPokemon"][0]
+    assert isinstance(poke_obj, Pokemon)
+    assert poke_obj.pokedex_id == 999000
+
+    # change egg to old one
+    computer["allPokemon"][0].update({
+        "caughtAt": "2023-10-21T02:58:50.117Z",
+    })
+    poke_obj, poke_bag = client.check_got_dragon_egg()
+    assert poke_bag is None
+    assert poke_obj is None
+
+    """
+    to test:
+    def check_got_dragon_egg(self):
+    def check_egg_hatched(self, current_buddy):
+    def check_pokebuddy(self, cached=False):
+    def set_buddy(self, pokemon):
+    def hatch_egg(self):
+    """
